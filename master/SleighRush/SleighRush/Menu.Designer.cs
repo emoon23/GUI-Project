7d66d0a490e13745c48a616de7d25fa5e3b9eddb
Darya.Kiktenko <darya.kiktenko@meau.com>
2018-12-16 17:01:55 -0600

    Minor cleanup

--- master/SleighRush/SleighRush/Menu.Designer.cs
+++ master/SleighRush/SleighRush/Menu.Designer.cs
@@ -29,8 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.Enter = new System.Windows.Forms.Button();
-            this.Exit = new System.Windows.Forms.Button();
+            this.ENTER = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -46,39 +45,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "SLEIGH RUSH";
             // 
-            // Enter
+            // ENTER
             // 
-            this.Enter.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Enter.Location = new System.Drawing.Point(676, 115);
-            this.Enter.Name = "Enter";
-            this.Enter.Size = new System.Drawing.Size(94, 47);
-            this.Enter.TabIndex = 1;
-            this.Enter.Text = "Enter";
-            this.Enter.UseVisualStyleBackColor = true;
-            this.Enter.Click += new System.EventHandler(this.Enter_Click);
-            // 
-            // Exit
-            // 
-            this.Exit.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Exit.Location = new System.Drawing.Point(676, 226);
-            this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(94, 43);
-            this.Exit.TabIndex = 2;
-            this.Exit.Text = "Exit";
-            this.Exit.UseVisualStyleBackColor = true;
-            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            this.ENTER.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ENTER.Font = new System.Drawing.Font("MITSUBISHI CNC Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ENTER.Location = new System.Drawing.Point(353, 202);
+            this.ENTER.Name = "ENTER";
+            this.ENTER.Size = new System.Drawing.Size(94, 47);
+            this.ENTER.TabIndex = 1;
+            this.ENTER.Text = "ENTER";
+            this.ENTER.UseVisualStyleBackColor = true;
+            this.ENTER.Click += new System.EventHandler(this.Enter_Click);
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::SleighRush.Properties.Resources.santa_t;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Exit);
-            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.ENTER);
             this.Controls.Add(this.label1);
             this.Name = "Menu";
             this.Text = "Menu";
@@ -90,7 +75,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button Enter;
-        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button ENTER;
     }
 }
--- master/SleighRush/SleighRush/Menu.cs
+++ master/SleighRush/SleighRush/Menu.cs
@@ -19,13 +19,22 @@ namespace SleighRush
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+
+            //Positioning the "Enter" button in the middle of the screen - regardless of screen size
+            ENTER.Top = (this.Height / 2) - (ENTER.Height / 2);
+            ENTER.Left = (this.Width / 2) - (ENTER.Width / 2);
+
+            //Positioning the "Sleigh Rush" label in the middle of the screen, 33 pixels from the top of the screen - regardless of screen size
+            label1.Top = 33;
+            label1.Left = (this.Width / 2) - (label1.Width / 2);
         }
 
         private void Enter_Click(object sender, EventArgs e)
         {
+            this.Hide();
             GameScreen sc = new GameScreen();
-            sc.Show();
-
+            sc.ShowDialog();
+            this.Close();
         }
 
         private void Exit_Click(object sender, EventArgs e)
